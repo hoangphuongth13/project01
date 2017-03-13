345cfad435618c4c6d9950c7909c09ffef9130bf
nga nguyen <nguyenthiennga.mingo@outlook.com.vn>
2017-03-13 21:37:54 +0700

    nga

--- testcode02/Form1.cs
+++ testcode02/Form1.cs
@@ -17,5 +17,10 @@ namespace testcode02
             InitializeComponent();
            
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //nga
+        }
     }
 }
